8f109b13092ce6e9255c73162eb9eeb23e71172d
Mathieu Steinmetz <mathieu.steinmetz22@gmail.com>
2019-03-14 10:32:10 +0100

    twitter stream test

--- server/app.js
+++ server/app.js
@@ -1,20 +1,36 @@
 const Twitter = require('twitter');
 const config = require('./config.js');
+const express = require('express')
+const socket = require('socket.io')
+const fs = require('fs')
+const app = express()
+const port = 3000
 
 const twitter = new Twitter(config)
 var params = {
-    q: '#Lannion',
-    count: 10,
+    q: '#macron',
+    count: 1,
     result_type: 'recent',
-    lang: 'fr'
+    lang: 'fr',
+    tweet_mode: 'extended'
 }
 
 twitter.get('search/tweets', params, function (err, data, response) {
-    if (!err) {
-        for (tweet of data.statuses) {
-            console.log(tweet.text);
-        }
-    } else {
-        console.log(err);
-    }
+    if (err) return err
+
 })
+
+var stream = twitter.stream('statuses/filter', {
+    track: 'javascript'
+});
+stream.on('data', function (event) {
+    fs.writeFile('test.json', JSON.stringify(event, null, 4), () => {
+        if (event.retweeted_status !== undefined) {
+            console.log('File wrote');
+        } else {
+            console.log('Its not a RT');
+
+        }
+    });
+});
+app.listen(port, () => console.log(`Listening on port ${port}!`))
