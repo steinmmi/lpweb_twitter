5f23c26af0c3b840f19a9c2a91d935084f7b75ca
Mathieu Steinmetz <mathieu.steinmetz22@gmail.com>
2019-03-15 13:48:10 +0100

    Use Twit instead of Twitter

--- server/app.js
+++ server/app.js
@@ -1,37 +1,17 @@
-const Twitter = require('twitter');
-const config = require('./config.js');
+const streamService = require('./src/stream')
 const app = require('express')()
 const http = require('http').Server(app);
 const io = require('socket.io')(http)
-
-const fs = require('fs')
 const port = 3000
 
-const twitter = new Twitter(config)
-var params = {
-    q: '#macron',
-    count: 1,
-    result_type: 'recent',
-    lang: 'fr',
-    tweet_mode: 'extended'
-}
-
 io.on('connection', function (socket) {
-    console.log('a user connected');
+    socket.on('new search', (s) => {
+        streamService.newSearch(s, (event) => {
+            io.emit('new tweet', event)
+        })
+    })
 });
-
-twitter.get('search/tweets', params, function (err, data, response) {
-    if (err) return err
-
-})
-
-var stream = twitter.stream('statuses/filter', {
-    track: 'javascript'
-});
-stream.on('data', function (event) {
-    console.log("oui");
-
+streamService.newSearch('love', (event) => {
     io.emit('new tweet', event)
-});
-
+})
 http.listen(port, '0.0.0.0')
---
+++ server/src/stream.js
@@ -0,0 +1,20 @@
+const Twit = require('twit')
+const config = require('../config.js');
+const T = new Twit(config)
+let stream
+
+function newSearch(str, _callback) {
+    if (stream)
+        stream.stop()
+    stream = T.stream('statuses/filter', {
+        track: str
+    });
+
+    stream.on('tweet', function (event) {
+        _callback(event)
+    });
+}
+
+module.exports = {
+    newSearch
+}
